21dc76d99b7468b8bcaf2f52b53dd1647751be85
Abhishek <abhishekkumarshah2.0@gmail.com>
2025-11-11 08:55:47 +0000

    the text color of "out of stock" , "need refill" and "in stock" must be

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -13,12 +13,12 @@ const badgeVariants = cva(
         secondary:
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         destructive:
-          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+          "border-transparent bg-destructive text-black hover:bg-destructive/80",
         outline: "text-foreground",
         success:
-          "border-transparent bg-green-500 text-primary-foreground hover:bg-green-500/80",
+          "border-transparent bg-green-500 text-black hover:bg-green-500/80",
         warning:
-          "border-transparent bg-yellow-500 text-primary-foreground hover:bg-yellow-500/80",
+          "border-transparent bg-yellow-500 text-black hover:bg-yellow-500/80",
       },
     },
     defaultVariants: {
